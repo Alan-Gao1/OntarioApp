0f66b2268bba589e1b189d0a57182f970b7fa44b
nbaroni <nbaroni@bayviewglen.ca>
2023-04-26 09:30:04 -0400

    me (mostly stefano) relinked the home page for us TEDDY

---
+++ Ontariapp/src/views/Homepage.vue
@@ -0,0 +1,381 @@
+<template>
+
+    <!-- nav -->
+    <nav class="navbar has-background-light" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/homepage.html">
+            <img src="/images/logo.png">
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+            <a class="navbar-item" href="../homepage/homepage.html">
+                Home
+            </a>
+
+            <a class="navbar-item" href="../search uni/search-uni.html">
+                Search for universities
+            </a>
+
+
+            </div>
+
+            <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                <a class="button is-info" href="../signup.html">
+                    <strong>Sign up</strong>
+                </a>
+                <a class="button has-background-primary has-text-white" href="../login.html">
+                    <strong>Log in</strong>
+                </a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </nav>
+
+      <!-- cover -->
+      <h1 class="heading">Find Your<br>Calling</h1>
+      <div class="container">
+          <div class="child-container">
+              <img src="https://picsum.photos/100" alt="pic 1">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/200" alt="pic 2">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/300" alt="pic 3">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/400" alt="pic 4">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/500" alt="pic 5">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/600" alt="pic 6">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/150" alt="pic 7">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/250" alt="pic 8">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/350" alt="pic 9">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/450" alt="pic 10">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/550" alt="pic 11">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/650" alt="pic 12">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/110" alt="pic 13">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/210" alt="pic 14">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/310" alt="pic 15">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/410" alt="pic 16">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/510" alt="pic 17">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/610" alt="pic 18">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/125" alt="pic 19">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/225" alt="pic 20">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/325" alt="pic 21">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/425" alt="pic 22">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/525" alt="pic 23">
+          </div>
+          <div class="child-container">
+              <img src="https://picsum.photos/625" alt="pic 24">
+          </div>
+      </div>
+
+
+    <!-- what we do -->
+    <div class="header-image">
+        <figure class="image">
+            <div class="is-overlay" style="margin: auto;width: fit-content;height: fit-content;">
+              <h1 id="covertext-title">What We Do</h1>
+              <p id="covertext-description">With a unique set of features, OntarioApp offers high school students a streamlined and centralized Ontario university application experience.</p>
+            </div>
+            
+            <img src="/images/uniHomePg.png">
+        </figure>  
+    </div>
+
+
+    <!-- hear what students divider -->
+    
+    <div>
+
+          <!-- <p class="title has-text-centered">
+            Why OntarioApp?
+          </p>
+          <p class="subtitle has-text-centered"> -->
+            <!-- Our unique set of features will make Ontario university applications a breeze -->
+            <!-- Hear what students have to say on social media!
+          </p> -->
+          <figure class="image">
+            <div class="is-overlay" style="margin: auto;width: fit-content;height: fit-content;">
+              <h1 id="why-oa-title">
+                <centre>Why OntarioApp?</centre>
+            </h1>
+              <p id="why-oa-description">
+                <centre>
+                    Hear what students have to say on social media!
+                </centre>
+              </p>
+            </div>
+            
+            <img src="/images/blue-rectangle.png">
+          </figure>  
+
+      </div>
+
+      <!-- student testimonials -->
+        <section class="section">
+                <div class="card">
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-left">
+                          <figure class="image is-48x48">
+                            <img class="is-rounded" src="/images/student.jpg" alt="Placeholder image">
+                          </figure>
+                        </div>
+                        <div class="media-content">
+                          <p class="title is-4">Bob Jones</p>
+                          <p class="subtitle is-6">@bobjones</p>
+                        </div>
+                      </div>
+                  
+                      <div class="content">
+                        OntarioApp's unique features made my application process a breeze. I definitely recommend! <a href="#">#ontarioapp</a> <a href="#">#university</a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card mt-4">
+                    <div class="card-content">
+                      <div class="media">
+                        <div class="media-left">
+                          <figure class="image is-48x48">
+                            <img class="is-rounded" src="/images/student2.jpg" alt="Placeholder image">
+                          </figure>
+                        </div>
+                        <div class="media-content">
+                          <p class="title is-4">Emma Smith</p>
+                          <p class="subtitle is-6">@emmasmith</p>
+                        </div>
+                      </div>
+                  
+                      <div class="content">
+                        What a great plaform! Everything is so simple and centralized. No more need for multiple portals and having to remember complex student codes. <a href="#">#applications</a> <a href="#">#ontarioapp</a>
+                      </div>
+                    </div>
+                  </div>
+
+        </section>
+
+        <!-- footer -->
+    <footer>
+          <figure class="image">
+            <div class="is-overlay" style="margin: auto;width: fit-content;height: fit-content;">
+              <p id="footer-text">
+                
+                  <strong>OntarioApp</strong> by Teddy Porfiris, Alan Gao, Nathan Baroni, and Grant Bourne.
+                  Visit our <a href="https://github.com/Alan-Gao1/OntarioApp">GitHub page</a>!
+                  </p>
+                
+            </div>
+            
+            <img src="/images/light-rectangle.png">
+          </figure>  
+        </footer>
+
+
+
+</template>
+
+<style scoped>
+
+
+#covertext-title{
+    color: white;
+    font-weight: bolder;
+    font-size: 9vw;
+    margin-left: 22vw;
+  }
+
+  #covertext-description{
+    color: white;
+    /* font-weight: bolder; */
+    font-size: 3vw;
+    margin-left: 2vw;
+  }
+
+  #why-oa-title{
+    color: white;
+    font-weight: bolder;
+    font-size: 4vw;
+    margin-left: 4vw;
+  }
+
+  #why-oa-description{
+    color: white;
+    /* font-weight: bolder; */
+    font-size: 2vw;
+  }
+  
+  #footer-text{
+    font-size: 1.5vw;
+    padding: 8vw;
+  }
+  
+  * {
+    box-sizing: border-box;
+    transition: all 0.2s;
+  }
+  
+  body {
+    margin: 0px;
+    overflow: hidden;
+    /* font-family: "Varela Round", sans-serif; */
+  }
+  
+  body .heading {
+    font-size: 8rem;
+    margin: 7rem 3rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-weight: 300;
+    color: #209CEE;
+    display: inline-block;
+    position: relative;
+    z-index: 3;
+  }
+  
+  body .heading::after {
+    content: "";
+    width: 80%;
+    height: 80%;
+    position: absolute;
+    top: 22%;
+    left: 12%;
+    border: 1px solid #00D1B2;
+    z-index: -20;
+  }
+  
+  @keyframes move {
+    from {
+      transform: rotate(45deg) translate(-120px, 60px);
+    }
+    to {
+      transform: rotate(45deg) translate(-120px, -290px);
+    }
+  }
+  body .container {
+    margin: 0px;
+    position: fixed;
+    right: 0px;
+    top: 0px;
+    display: grid;
+    grid-template-columns: auto auto auto;
+    gap: 1rem;
+    animation: move 4.5s linear infinite alternate;
+  }
+  
+  .container .child-container {
+    width: 150px;
+    height: 150px;
+    border-radius: 5px;
+    background-color: #b9b9b9;
+    text-align: center;
+    overflow: hidden;
+  }
+  
+  .child-container img {
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+  }
+  
+  @media screen and (max-width: 1000px) {
+    body .heading {
+      margin: 9rem 3rem;
+      font-size: 6rem;
+    }
+  }
+  
+  @media screen and (max-width: 780px) {
+    body .heading {
+      font-size: 4rem;
+      margin: 12rem 2rem;
+    }
+    @keyframes move {
+      from {
+        transform: rotate(45deg) translate(20px, 80px);
+      }
+      to {
+        transform: rotate(45deg) translate(20px, 20px);
+      }
+    }
+  
+    body .container {
+      gap: 0.5rem;
+    }
+    .container .child-container {
+      width: 100px;
+      height: 100px;
+      border-radius: 4px;
+    }
+  }
+  
+  @media screen and (max-width: 500px) {
+    body .heading {
+      margin: 8rem 2rem;
+    }
+  
+    @keyframes move {
+      from {
+        transform: rotate(45deg) translate(150px, 80px);
+      }
+      to {
+        transform: rotate(45deg) translate(150px, 20px);
+      }
+    }
+  }
+  
+
+
+
+
+</style>
