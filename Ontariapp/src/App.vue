83454d0ca260095cb5511c67e6a8d98b029800df
Grant Bourne <grant@rbgn.ca>
2023-05-10 09:12:15 -0400

    hamburger that works but it looks funny but thats #notmyproblem

--- Ontariapp/src/App.vue
+++ Ontariapp/src/App.vue
@@ -7,41 +7,59 @@ import HelloWorld from './components/HelloWorld.vue'
   <nav class="navbar has-background-light" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <router-link to="/" class="navbar-item"><img src="/images/logo.png"></router-link>
- 
 
-    
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                </a>
+                <nav class="navbar is-light navbar-end" role="navigation" aria-label="main navigation">
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="isOpen = !isOpen" v-bind:class="{'is-active': isOpen}">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </nav>
             </div>
     
-            <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-menu" v-bind:class="{'is-active': isOpen}">
                 <div class="navbar-start">
-                    <router-link to="/" class="navbar-item">Home</router-link>
-
-<router-link to="/search" class="navbar-item">Search for Universities</router-link>
-    
-    
+                    <router-link to="/" class="navbar-item is-tab">Home</router-link>
+                    <router-link to="/search" class="navbar-item is-tab">Search for Universities</router-link>
                 </div>
-    
+
                 <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                    <router-link to="/signup" class="button is-info">
-                        <strong>Sign up</strong>
-                    </router-link>
-                    <router-link to="/login" class="button has-background-primary has-text-white">
-                        <strong>Log in</strong>
-                    </router-link>
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <router-link to="/signup" class="button is-info">
+                                <strong>Sign up</strong>
+                            </router-link>
+                            <router-link to="/login" class="button has-background-primary has-text-white">
+                                <strong>Log in</strong>
+                            </router-link>
+                        </div>
                     </div>
                 </div>
-                </div>
             </div>
+
             </nav>
   <RouterView />
 </template>
+
+<script>
+    export default {
+        data() { 
+            return {
+                isOpen: false
+            }
+        },
+        methods: { 
+            onload() {
+                
+            }
+        },
+        beforeMount() {
+            this.onload()
+        }
+    };
+    
+</script>
+
 
 <style scoped>
 @import 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css';
