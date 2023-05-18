9a78414ff237f81eab956a6cf6364a342acc0abb
Grant Bourne <grant@rbgn.ca>
2023-05-18 09:23:03 -0400

    fix docs

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+  },
+  build: {
+    outDir: 'docs'
   }
 })
+
+
+// module.exports = {
+//   outputDir: path.resolve(__dirname, "./docs/")
+// }
