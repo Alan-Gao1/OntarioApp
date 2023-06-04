dc5da1023b67fb1777a26209bfd059a72591817e
Alan Gao <agao1@bayviewglen.ca>
2023-06-04 17:51:52 -0400

    W

--- client/src/views/SelectUniApplication.vue
+++ client/src/views/SelectUniApplication.vue
@@ -1,12 +1,20 @@
 <template>
     <section class="section">
         <div class="columns is-multiline">
-            <UniApplyCard v-for="university in universities" :key=university :icon=university.Logo :name=university.Name :site=university.WebsiteLink :uniId=university.UniversityID />
+            <!-- <UniApplyCard v-for="university in universities" :key=university :icon=university.Logo :name=university.Name :site=university.WebsiteLink :uniId=university.UniversityID /> -->
+            <UniApplyCard v-for="i in 4" :key=universities[i-1] :icon=universities[i-1].Logo :name=universities[i-1].Name :site=universities[i-1].WebsiteLink :uniId=universities[i-1].UniversityID />
         </div>
+        <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+            <a class="pagination-previous" onclick="iterate(-4, i)">Previous</a>
+            <a class="pagination-next" onclick="iterate(4, i)">Next page</a>
+        </nav>
     </section>
 </template>
 
 <script>
+function iterate(num, i){
+    i = i+num;
+}
 export default {
     data() {
         return {
