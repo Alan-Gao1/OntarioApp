48de09098cf1167ca64fd2322576ed0f1c3ee17c
Grant Bourne <grant@rbgn.ca>
2023-06-04 18:26:17 -0400

    add pagination

--- client/src/views/SelectUniApplication.vue
+++ client/src/views/SelectUniApplication.vue
@@ -1,32 +1,36 @@
 <template>
     <section class="section">
         <div class="columns is-multiline">
-            <!-- <UniApplyCard v-for="university in universities" :key=university :icon=university.Logo :name=university.Name :site=university.WebsiteLink :uniId=university.UniversityID /> -->
-            <UniApplyCard v-for="i in 4" :key=universities[i-1] :icon=universities[i-1].Logo :name=universities[i-1].Name :site=universities[i-1].WebsiteLink :uniId=universities[i-1].UniversityID />
+            <UniApplyCard v-for="university in filteredUnis" :key=university :icon=university.Logo :name=university.Name :site=university.WebsiteLink :uniId=university.UniversityID />
         </div>
         <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-            <a class="pagination-previous" onclick="iterate(-4, i)">Previous</a>
-            <a class="pagination-next" onclick="iterate(4, i)">Next page</a>
+            <a v-bind:class="page<=0 ? 'pagination-previous is-disabled' : 'pagination-previous'" @click="page--">Previous</a>
+            <a v-bind:class="page>=3 ? 'pagination-next is-disabled' : 'pagination-next'" @click="page++">Next page</a>
         </nav>
     </section>
 </template>
 
 <script>
-function iterate(num, i){
-    i = i+num;
-}
 export default {
     data() {
         return {
-            universities: []
+            universities: [],
+            page: 0
         }
     },
     created() {
         let uri = 'http://127.0.0.1:8000/universities';
 
         this.axios.get(uri).then(res => {
-          this.universities = res.data;
+            this.universities = res.data.sort((a, b) => a.Name.localeCompare(b.Name));
         })
+    },
+    computed: {
+        filteredUnis() {
+            if (this.page < 0) this.page = 0
+            else if (this.page > 3) this.page = 3;
+            return this.universities.slice((this.page*8), (this.page*8)+8);
+        }
     }
 }
 </script>
